838a0c47bc2a6f537afc591fdca72e21c4896efc
souravkhilari-netizen <sourav_khilari@epam.com>
2026-02-07 12:05:33 +0530

    Add square function and log its result

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function greet(name) {
 
 const userName = "World";
 console.log(greet(userName));
-
+const square = (x) => x * x;
+console.log("Square of 5:", square(5));
 const numbers = [1, 2, 3, 4, 5];
 const sum = numbers.reduce((total, n) => total + n, 0);
 console.log("Sum:", sum);
